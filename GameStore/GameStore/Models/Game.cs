ea971a7bc185b4d37f7266926b46c2d4535bc4d5
Hubert Knochowski <Knochos@gmail.com>
2018-01-24 23:52:27 +0100

    Rozgrzebana klasa Game.cs

---
+++ GameStore/GameStore/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public int GameTypeId { get; set; }
+        public string GameName { get; set; }
+        public string GameDescription { get; set; }
+        public DateTime GamePremiere { get; set; }
+        public int GameRating { get; set; }
+        public bool OrToBuy { get; set; }
+
+        public virtual GameType GameType { get; set; }
+    }
+}

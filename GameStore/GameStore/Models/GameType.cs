e5dd86ddb5cdff0bdb27bdc4c6ed243b8ea0bdd1
Hubert Knochowski <Knochos@gmail.com>
2018-01-25 21:13:03 +0100

    Dodanie klasy GameType i rozgrzebana klasa SalesInvoice

---
+++ GameStore/GameStore/Models/GameType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GameStore.Models
+{
+    public class GameType
+    {
+        public int GameTypeId { get; set; }
+        public string NameGameType { get; set; }
+        public string DescriptionGameType { get; set; }
+        public string FileGameType { get; set; }
+
+        public virtual ICollection<Game> Games { get; set; }
+    }
+}
---
+++ GameStore/GameStore/Models/SalesInvoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.Models
+{
+    public class SalesInvoice
+    {
+        public int SalesInvoiceId { get; set; }
+        public int CodeSalesInvoice { get; set; }
+        public decimal Price { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+
+
+    }
+
+    public enum OrderStatus
+    {
+        Czeka_na_płatność,
+        Zakończone
+    }
+}

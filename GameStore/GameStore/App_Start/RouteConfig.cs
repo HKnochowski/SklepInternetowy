49dd028cd8e8bd1b7ca73d55644afd525af013ac
Hubert Knochowski <Knochos@gmail.com>
2018-01-28 19:05:15 +0100

    Poprawienie RouteConfig.cs

--- GameStore/GameStore/App_Start/RouteConfig.cs
+++ GameStore/GameStore/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace GameStore
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "StaticSite",
+                url: "site/{nazwa}.html",
+                defaults: new { Controller = "Home", action = "StaticSite" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }

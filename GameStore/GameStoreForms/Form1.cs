683b6fd9e45d2087c31256c37630c199a39903a4
nerczu <35469986+nerczu@users.noreply.github.com>
2018-01-30 23:34:32 +0100

    Update
    Utworzenie dwóch nowych porjektów + modyfikacja klasy Game

---
+++ GameStore/GameStoreForms/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameStoreForms.ServiceReference1;
+using GameStoreForms;
+
+namespace GameStoreForms
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Game G = new Game();
+            G.GameId = Convert.ToInt32(textId.Text);
+            G.GameName = textName.Text;
+            G.GameDescription = textDescription.Text;
+            G.GamePremiere = Convert.ToDateTime(textPremiere.Text);
+            G.GameRating = Convert.ToInt32(textRating.Text); ;
+            G.OrToBuy = Convert.ToBoolean(textOrToBuy.Text);
+            //G.GameType = textType.Text;
+            G.GameTypeId = Convert.ToInt32(textTypeId.Text); ;
+
+            Service1Client service = new Service1Client();
+
+            if (service.InsertGame(G) == 1)
+            {
+                MessageBox.Show("Gra dodana");
+            }
+        }
+    }
+}
